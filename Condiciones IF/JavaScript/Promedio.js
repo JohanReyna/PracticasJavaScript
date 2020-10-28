331f18c286b7792cab3591e7776c80125147f2fa
JohanReyna <johanreyna1508@gmail.com>
2020-10-28 09:58:43 -0600

    PRACTICA PROMEDIO FINALIZADA

--- Condiciones IF/JavaScript/Promedio.js
+++ Condiciones IF/JavaScript/Promedio.js
@@ -12,11 +12,39 @@ function CalcularPromedio(){
     
     var Cal4 = Number(document.getElementById("cal4").value);
 
-    var Resultado = Cal1 + Cal2 + Cal3 + Cal4
+    var PrintResultado2 = document.getElementById("ResultadoF2")
 
     var PrintResultado = document.getElementById("ResultadoF");
 
-    if (Resultado = Cal1 + Cal2 + Cal3 + Cal4){
-        PrintResultado.innerText = Resultado / 4
+    var valorminimo, Resultado;
+
+
+    if(Cal1 < Cal2 && Cal1 < Cal3 && Cal1 < Cal4){
+        valorminimo = Math.min(Cal1, Cal2, Cal3, Cal4);
+        PrintResultado2.innerText = "El Numero Minimo es " + valorminimo;
+        Resultado = (Cal2 + Cal3 + Cal4) / 3;
+        PrintResultado.innerText = "El Promedio es " + Resultado;
     }
+
+    if(Cal2 < Cal1 && Cal2 < Cal3 && Cal2 < Cal4){
+        valorminimo = Math.min(Cal1, Cal2, Cal3, Cal4);
+        PrintResultado2.innerText = "El Numero Minimo es " + valorminimo;
+        Resultado = (Cal1 + Cal3 + Cal4) /3;
+        PrintResultado.innerText = "El Promedio es " + Resultado;
+    }
+
+    if(Cal3 < Cal1 && Cal3 < Cal2 && Cal3 < Cal4){
+        valorminimo = Math.min(Cal1, Cal2, Cal3, Cal4);
+        PrintResultado2.innerText = "El Numero Minimo es " + valorminimo;
+        Resultado = (Cal1 + Cal2 + Cal4) / 3;
+        PrintResultado.innerText = "El Promedio es " + Resultado;
+    }
+
+    if(Cal4 < Cal1 && Cal4 < Cal2 && Cal4 < Cal3){
+        valorminimo = Math.min(Cal1, Cal2, Cal3, Cal4);
+        PrintResultado2.innerText = "El Numero Minimo es " + valorminimo;
+        Resultado = (Cal1 + Cal2 + Cal3) / 3;
+        PrintResultado.innerText = "El Promedio es " + Resultado;
+    }
+
 }
